61f89c8f680c01e66e777124727a612d2ba6b4d1
Lucija Cabai <lucija.cabai@gmail.com>
2022-08-17 16:43:07 +0200

    change to welcome view

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Jela Svijeta</title>
+</head>
+
+<body>
+  <h1>helou</h1>
+  <div> pick language (hr/en/fr) <a href="http://127.0.0.1:8000/api/meals?lang=hr"> http://127.0.0.1:8000/api/meals?lang=hr</a> </br>
+    filter by category id (1-10) <a href="http://127.0.0.1:8000/api/meals?lang=hr&category=1"> http://127.0.0.1:8000/api/meals?lang=hr&category=1</a> </br>
+    filter by tag ids (1-10) <a href="http://127.0.0.1:8000/api/meals?lang=hr&tags=1,2,3"> http://127.0.0.1:8000/api/meals?lang=hr&tags=1,2,3</a> </br>
+    or do all the things (primjer iz zadatka) <a href="http://127.0.0.1:8000/api/meals?per_page=5&tags=2&lang=hr&with=ingredients,category,tags&diff_
+time=1493902343&page=2"> http://127.0.0.1:8000/api/meals?per_page=5&tags=2&lang=hr&with=ingredients,category,tags&diff_
+      time=1493902343&page=2</a>
+  </div>
+</body>
+
+</html>
